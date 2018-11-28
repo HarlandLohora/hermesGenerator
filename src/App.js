1f5a405c0442bada51d5597a2b5f431d9e9b6488
HarlandLohora <lohoraharland@gmail.com>
2018-11-28 11:51:37 -0600

    Creando Componente Dashboard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Main from './Components/Main/Main';
+import Dashboard from './Components/Dashboard/Dashboard';
 import './App.css';
 class App extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Main}/>
+          <Route exact path="/" component = {Main}/>
+          <Route path="/dashboard" component = {Dashboard}/>
         </Switch>
       </Router>
     );
